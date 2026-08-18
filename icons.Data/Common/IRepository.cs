548e4d3f5e19f577b1c5fdb638d407b0eb38a763
Blueonn33 <m.marinov1733@gmail.com>
2026-08-18 19:25:44 +0300

    generic interface

--- icons.Data/Common/IRepository.cs
+++ icons.Data/Common/IRepository.cs
@@ -1,7 +1,12 @@
 ﻿namespace icons.Data.Common
 {
-    public interface IRepository<TEntity> where TEntity : class
+    public interface IRepository<TEntity> where TEntity : class, IEntity
     {
-
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        Task SaveAsync();
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
     }
 }
