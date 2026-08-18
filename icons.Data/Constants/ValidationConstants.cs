d1eafd86fe9cde71a476f81874e81354dab4154b
Blueonn33 <m.marinov1733@gmail.com>
2026-08-18 22:14:05 +0300

    seed reviews

--- icons.Data/Constants/ValidationConstants.cs
+++ icons.Data/Constants/ValidationConstants.cs
@@ -7,7 +7,7 @@
         public const int IconTitleMaxLength = 50;
         public const int IconDescriptionMinLength = 15;
         public const int IconDescriptionMaxLength = 2000;
-        public const int IconImageUrlLength = 500;
+        public const int IconImageUrlLength = 900;
         public const double IconAverageRangeMinValue = 1.0;
         public const double IconAverageRangeMaxValue = 5.0;
         public const int IconUsernameMinLength = 3;
@@ -19,12 +19,12 @@
         public const int ReviewTitleMaxLength = 50;
         public const int ReviewDescriptionMinLength = 15;
         public const int ReviewDescriptionMaxLength = 2000;
-        public const int ReviewUserProfilePictureUrlLength = 500;
+        public const int ReviewUserProfilePictureUrlLength = 900;
         public const int ReviewUsernameMinLength = 3;
         public const int ReviewUsernameMaxLength = 50;
 
 
         // User
-        public const int UserProfilePictureUrlLength = 500;
+        public const int UserProfilePictureUrlLength = 900;
     }
 }
