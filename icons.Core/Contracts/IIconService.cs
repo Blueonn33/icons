2d73e3b4264c8eb2ee7628bff767b082d1865f43
Blueonn33 <m.marinov1733@gmail.com>
2026-08-19 12:58:39 +0300

    services

---
+++ icons.Core/Contracts/IIconService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace icons.Core.Contracts
+{
+    public interface IIconService
+    {
+    }
+}
---
+++ icons.Core/Contracts/IReviewService.cs
@@ -0,0 +1,6 @@
+﻿namespace icons.Core.Contracts
+{
+    public interface IReviewService
+    {
+    }
+}
