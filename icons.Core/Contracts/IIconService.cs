e43fff6eba168bad1da5f8587295c07814e931c2
Blueonn33 <m.marinov1733@gmail.com>
2026-08-21 22:58:11 +0300

    create icon dto

--- icons.Core/Contracts/IIconService.cs
+++ icons.Core/Contracts/IIconService.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using icons.Data.Models;
 
 namespace icons.Core.Contracts
 {
     public interface IIconService
     {
+        Task<IEnumerable<Icon>> GetAllIconsAsync();
+        Task<IEnumerable<Icon>> GetAllIconsByUserIdAsync(string userId);
+        Task<IEnumerable<Icon>> GetTop3IconsAsync();
+        Task<Icon> GetIconByIdAsync(int id);
+        Task AddIconAsync(Icon icon);
+        void UpdateIcon(Icon icon);
+        void DeleteIcon(Icon icon);
     }
 }
---
+++ icons.Core/Dtos/Icon/CreateIconDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static icons.Data.Constants.ValidationConstants;
+
+namespace icons.Core.Dtos.Icon
+{
+    public class CreateIconDto
+    {
+        [Required]
+        [StringLength(IconImageUrlLength)]
+        public string ImageUrl
+        {
+            get; set;
+        } = null!;
+
+        [Required]
+        [MinLength(IconTitleMinLength)]
+        [MaxLength(IconTitleMaxLength)]
+        public string Title
+        {
+            get; set;
+        } = null!;
+
+        [MinLength(IconDescriptionMinLength)]
+        [MaxLength(IconDescriptionMaxLength)]
+        public string? Description
+        {
+            get; set;
+        }
+
+        [Required]
+        public string UserId
+        {
+            get;
+            set;
+        } = null!;
+    }
+}
