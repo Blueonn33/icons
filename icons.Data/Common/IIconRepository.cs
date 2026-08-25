26a21dd1d13f9fcd587a23dc873e67f64e9a889d
Blueonn33 <m.marinov1733@gmail.com>
2026-08-25 19:21:34 +0300

    IconRepository

---
+++ icons.Data/Common/IIconRepository.cs
@@ -0,0 +1,12 @@
+﻿using icons.Data.Models;
+
+namespace icons.Data.Common
+{
+    public interface IIconRepository : IRepository<Icon>
+    {
+        Task<IEnumerable<Icon>> GetAllIconsAsync();
+        Task<IEnumerable<Icon>> GetAllIconsByUserIdAsync(string userId);
+        Task<IEnumerable<Icon>> GetTop3IconsAsync();
+        Task<Icon> GetIconWithReviewsByIdAsync(int id);
+    }
+}
--- icons.Data/Common/IconRepository.cs
+++ icons.Data/Common/IconRepository.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace icons.Data.Common
 {
-    public class IconRepository : Repository<Icon>
+    public class IconRepository : Repository<Icon>, IIconRepository
     {
         private readonly ApplicationDbContext _context;
         public IconRepository(ApplicationDbContext context) : base(context)
@@ -14,6 +14,31 @@ namespace icons.Data.Common
         public async Task<IEnumerable<Icon>> GetAllIconsAsync()
         {
             return await _context.Icons.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Icon>> GetAllIconsByUserIdAsync(string userId)
+        {
+            return await _context.Icons
+                .AsNoTracking()
+                .Where(i => i.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<Icon> GetIconWithReviewsByIdAsync(int id)
+        {
+            return await _context.Icons
+                .AsNoTracking()
+                .Include(i => i.Reviews)
+                .FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<IEnumerable<Icon>> GetTop3IconsAsync()
+        {
+            return await _context.Icons
+                .AsNoTracking()
+                .OrderByDescending(i => i.AverageRating)
+                .Take(3)
+                .ToListAsync();
         }
     }
 }
