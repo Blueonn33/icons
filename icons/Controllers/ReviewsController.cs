7497148baa2a71282595e3a24885e065135af529
Blueonn33 <m.marinov1733@gmail.com>
2026-08-28 17:31:45 +0300

    comment styles

--- icons/Controllers/ReviewsController.cs
+++ icons/Controllers/ReviewsController.cs
@@ -1,6 +1,4 @@
 ﻿using icons.Core.Contracts;
-using icons.Core.Dtos.Review;
-using icons.Models.Reviews;
 using Microsoft.AspNetCore.Mvc;
 
 namespace icons.Controllers
@@ -12,29 +10,6 @@ namespace icons.Controllers
         public ReviewsController(IReviewService service)
         {
             _service = service;
-        }
-
-        public async Task<IActionResult> GetReviews(int id)
-        {
-            var reviews = await _service.GetAllReviewsByIconIdAsync(id);
-
-            var reviewDtos = reviews.Select(r => new ReviewGetDto
-            {
-                Id = r.Id,
-                Title = r.Title,
-                Description = r.Description,
-                PublishedTime = r.PublishedTime,
-                Rating = r.Rating,
-                Username = r.Username,
-                UserProfilePictureUrl = r.UserProfilePictureUrl
-            }).ToList();
-
-            var model = new ReviewsViewModel
-            {
-                Reviews = reviewDtos
-            };
-
-            return PartialView("_Reviews", model);
         }
     }
 }
