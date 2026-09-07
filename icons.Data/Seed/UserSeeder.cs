3d3415d5356eeac4e71b2f203ebabdec301c1cf7
Blueonn33 <m.marinov1733@gmail.com>
2026-09-07 12:21:28 +0300

    secrets

--- icons.Data/Seed/UserSeeder.cs
+++ icons.Data/Seed/UserSeeder.cs
@@ -18,28 +18,8 @@ namespace icons.Data.Seed
             var adminElixir = configuration["AdminUser:Elixir"];
             var adminRank = configuration["Admin:Rank"];
 
-            var user1Email = configuration["SeedUser1:Email"];
-            var user1Password = configuration["SeedUser1:Password"];
-            var user1Name = configuration["SeedUser1:Name"];
-            var user1ProfilePictureUrl = configuration["SeedUser1:ProfilePictureUrl"];
-            var user1Elixir = configuration["SeedUser1:Elixir"];
-            var user1Rank = configuration["SeedUser1:Rank"];
-
-            var user2Email = configuration["SeedUser2:Email"];
-            var user2Password = configuration["SeedUser2:Password"];
-            var user2Name = configuration["SeedUser2:Name"];
-            var user2ProfilePictureUrl = configuration["SeedUser2:ProfilePictureUrl"];
-            var user2Elixir = configuration["SeedUser2:Elixir"];
-            var user2Rank = configuration["SeedUser2:Rank"];
-
             await CreateUserWithRole(userManager, adminEmail, adminPassword, adminName, adminProfilePictureUrl, adminElixir, adminRank,
                 Roles.Admin);
-
-            await CreateUserWithRole(userManager, user1Email, user1Password, user1Name, user1ProfilePictureUrl, user1Elixir, user1Rank,
-                Roles.User);
-
-            await CreateUserWithRole(userManager, user2Email, user2Password, user2Name, user2ProfilePictureUrl, user2Elixir, user2Rank,
-                Roles.User);
         }
 
         public static async Task CreateUserWithRole(UserManager<ApplicationUser> userManager, string email,
