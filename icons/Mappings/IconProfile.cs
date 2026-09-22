bd8b834ea81215a3b605be8fe663bc81f2c5b79a
Blueonn33 <m.marinov1733@gmail.com>
2026-09-22 18:36:33 +0300

    Добавяне на профил с mapping между IconGet и IconViewModel

---
+++ icons/Mappings/IconProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using icons.Core.Dtos.Icon;
+using icons.Models.Icons;
+
+namespace icons.Mappings
+{
+    public class IconProfile : Profile
+    {
+        public IconProfile()
+        {
+            CreateMap<IconGetDto, IconViewModel>();
+        }
+    }
+}
