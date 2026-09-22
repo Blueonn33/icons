ed7616ad3f9572519101dfc58899a744a570ea9d
Blueonn33 <m.marinov1733@gmail.com>
2026-09-23 00:05:11 +0300

    Review View model за потребителя

---
+++ icons/Models/Reviews/ReviewUserProfileViewModel.cs
@@ -0,0 +1,38 @@
+﻿using icons.Data.Enums;
+
+namespace icons.Models.Reviews
+{
+    public class ReviewUserProfileViewModel
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Title
+        {
+            get; set;
+        }
+            = null!;
+
+        public string? Description
+        {
+            get; set;
+        }
+
+        public EnumReviewRating? Rating
+        {
+            get; set;
+        }
+
+        public string UserId
+        {
+            get; set;
+        } = null!;
+
+        public int IconId
+        {
+            get; set;
+        }
+    }
+}
