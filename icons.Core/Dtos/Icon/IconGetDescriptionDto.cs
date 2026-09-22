6a8774a71b8d9fcaa75139f4202073aa82935569
Blueonn33 <m.marinov1733@gmail.com>
2026-09-22 18:35:44 +0300

    Icon description dto

---
+++ icons.Core/Dtos/Icon/IconGetDescriptionDto.cs
@@ -0,0 +1,58 @@
+﻿using icons.Core.Dtos.Review;
+
+namespace icons.Core.Dtos.Icon
+{
+    public class IconGetDescriptionDto
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string ImageUrl
+        {
+            get; set;
+        } = null!;
+
+        public string Title
+        {
+            get; set;
+        } = null!;
+
+        public string? Description
+        {
+            get; set;
+        }
+
+        public double AverageRating
+        {
+            get; set;
+        }
+
+        public DateTime PublishedTime
+        {
+            get; set;
+        }
+
+        public string Username
+        {
+            get; set;
+        } = null!;
+
+        public string UserProfilePictureUrl
+        {
+            get; set;
+        } = null!;
+
+        public string UserId
+        {
+            get; set;
+        } = null!;
+
+        public List<ReviewGetDto> Reviews
+        {
+            get;
+            set;
+        } = new List<ReviewGetDto>();
+    }
+}
