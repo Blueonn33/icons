5c700e70977583d75c1d1c241f106526681fb6f8
Blueonn33 <m.marinov1733@gmail.com>
2026-09-22 22:29:46 +0300

    избиране на емоджи при създаването на ревю

---
+++ icons/wwwroot/js/createReview.js
@@ -0,0 +1,12 @@
+﻿function changeImage(imgElement) {
+    document.querySelectorAll(".rating-img").forEach(img => {
+        const original = img.getAttribute("data-img");
+        img.src = "/img/" + original;
+    });
+
+    const original = imgElement.getAttribute("data-img");
+    imgElement.src = "/img/color-" + original;
+
+    const radio = imgElement.previousElementSibling;
+    radio.checked = true;
+}
