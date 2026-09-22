b7b7e9a188e8ceaf3646cd35d4cc83c39dd96d51
Blueonn33 <m.marinov1733@gmail.com>
2026-09-22 22:36:39 +0300

    review view model

---
+++ icons/Models/Reviews/ReviewViewModel.cs
@@ -0,0 +1,64 @@
+﻿using icons.Data.Enums;
+
+namespace icons.Models.Reviews
+{
+    public class ReviewViewModel
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Title
+        {
+            get; set;
+        }
+            = null!;
+
+        public string? Description
+        {
+            get; set;
+        }
+
+        public EnumReviewRating? Rating
+        {
+            get; set;
+        }
+
+        public DateTime PublishedTime
+        {
+            get; set;
+        }
+
+        public string UserProfilePictureUrl
+        {
+            get; set;
+        } = null!;
+
+        public string Username
+        {
+            get; set;
+        } = null!;
+
+        public string RankImageUrl
+        {
+            get;
+            set;
+        } = null!;
+
+        public EnumUserElixirRank Rank
+        {
+            get; set;
+        }
+
+        public string UserId
+        {
+            get; set;
+        } = null!;
+
+        public int IconId
+        {
+            get; set;
+        }
+    }
+}
